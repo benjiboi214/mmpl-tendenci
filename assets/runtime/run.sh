a3464737b52ce48d85a3768ab0f28ee46ddecdca
Ben Elliot <belliot@liveperson.com>
2021-01-19 15:02:43 +1100

    Make run do project setup

--- assets/runtime/run.sh
+++ assets/runtime/run.sh
@@ -1,15 +1,68 @@
 #! /bin/bash
 
+# Environment
+PIP=$(which pip3)
 PYTHON=$(which python3)
 
-function start_install()
+
+# Check directories and create them if necessary
+function check_dirs()
 {
-    echo "Installing Tendeci"  && echo ""
-    source "$TENDENCI_HOME/install.sh"
-    check_user
-    check_dirs
-    install_tendenci
-    create_cronjobs
+    for dir in $TENDENCI_INSTALL_DIR\
+        $TENDENCI_LOG_DIR;
+do
+    [ -d "$dir" ] || mkdir "$dir"
+    chown "$TENDENCI_USER:" "$dir"
+done
+}
+
+# Check user to run the application, create it if not done yet
+function check_user()
+{
+    if ! grep -i "^$TENDENCI_USER" /etc/passwd; then
+        useradd -b "$TENDENCI_HOME" -U "$TENDENCI_USER"
+    fi
+}
+
+function create_cronjobs()
+{
+
+    echo "Creating cronjobs" && echo ""
+    (crontab -l ; echo "30   2 * * * $PYTHON $TENDENCI_INSTALL_DIR/manage.py run_nightly_commands") | crontab -
+    (crontab -l ; echo "30   2 * * * $PYTHON $TENDENCI_INSTALL_DIR/manage.py process_unindexed") | crontab -
+
+}
+
+
+function create_tendenci_project()
+{
+    # Creating tendenci project
+    echo "Creating tendenci project" && echo ""
+    cd "$TENDENCI_INSTALL_DIR"
+    tendenci startproject "$APP_NAME" "$APP_NAME"
+    cd "$APP_NAME"
+    $PIP install  -r requirements/dev.txt --upgrade
+
+    #Install theme
+    echo "Installing theme" && echo ""
+    mkdir "$TENDENCI_PROJECT_ROOT"/themes/tendenci2020
+    cd "$TENDENCI_INSTALL_DIR"
+    PACKAGE_ORIGIN=$(pip3 show tendenci | grep Location:)
+    THEME_ORIGIN=${PACKAGE_ORIGIN//"Location: "/}"/tendenci/themes/t7-tendenci2020"
+    cd $THEME_ORIGIN
+    cp -r ./* "$TENDENCI_PROJECT_ROOT"/themes/tendenci2020
+
+    # Preparing directories
+    echo "Preparing directories" && echo ""
+    mkdir "$TENDENCI_PROJECT_ROOT"/static
+    chown "$TENDENCI_USER:" /var/log/"$APP_NAME"/
+    chmod -R -x+X,g+rw,o-rwx /var/log/"$APP_NAME"/
+    chown -R "$TENDENCI_USER:" "$TENDENCI_HOME"
+    chmod -R -x+X,g-w,o-rwx "$TENDENCI_PROJECT_ROOT"/
+    chmod +x                "$TENDENCI_PROJECT_ROOT"/manage.py
+    chmod -R ug-x+rwX,o-rwx "$TENDENCI_PROJECT_ROOT"/media/ 
+    chmod -R ug-x+rwX,o-rwx "$TENDENCI_PROJECT_ROOT"/themes/ 
+    chmod -R ug-x+rwX,o-rwx "$TENDENCI_PROJECT_ROOT"/whoosh_index/   
 }
 
 function setup_keys()
@@ -97,7 +150,10 @@ function run
 
 
 if [ ! -f "$TENDENCI_PROJECT_ROOT/conf/first_run" ]; then
-    start_install
+    check_user
+    check_dirs
+    create_tendenci_project
+    create_cronjobs
     setup_keys
     create_settings
     initial_setup
