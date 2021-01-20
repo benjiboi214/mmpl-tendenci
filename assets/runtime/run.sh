4a76427eb725912aafcced62acf94edfa29aeb1a
Ben Elliot <belliot@liveperson.com>
2021-01-21 01:26:50 +1100

    Remove stupid settings gen sit

--- assets/runtime/run.sh
+++ assets/runtime/run.sh
@@ -65,48 +65,6 @@ function create_tendenci_project()
     chmod -R ug-x+rwX,o-rwx "$TENDENCI_PROJECT_ROOT"/whoosh_index/   
 }
 
-function setup_keys()
-{
-    echo "Creating secret keys"  && echo ""
-
-    SECRET_KEY=${SECRET_KEY:-$(mcookie)}
-    SITE_SETTINGS_KEY=${SITE_SETTINGS_KEY:-$(mcookie)}
-    sed -i "s/^SECRET_KEY.*/SECRET_KEY='$SECRET_KEY'/" \
-       "$TENDENCI_PROJECT_ROOT/conf/settings.py"
-    echo "SECRET_KEY: $SECRET_KEY" && echo ""
-    sed -i "s/^SITE_SETTINGS_KEY.*/SITE_SETTINGS_KEY='$SITE_SETTINGS_KEY'/" \
-        "$TENDENCI_PROJECT_ROOT/conf/settings.py"
-    echo "SITE_SETTINGS_KEY: $SITE_SETTINGS_KEY" && echo ""
-}
-
-function create_settings
-{
-    echo "Creating settings"  && echo ""
-
-     sed -i "s/^#DATABASES\['default'\]\['NAME'\].*/DATABASES\['default'\]\['NAME'\] = '${DB_NAME:-tendenci}'/" \
-        "$TENDENCI_PROJECT_ROOT/conf/settings.py"
-
-     sed -i "s/^#DATABASES\['default'\]\['HOST'\].*/DATABASES\['default'\]\['HOST'\] = '${DB_HOST:-localhost}'/" \
-        "$TENDENCI_PROJECT_ROOT/conf/settings.py"
-
-     sed -i "s/^#DATABASES\['default'\]\['USER'\].*/DATABASES\['default'\]\['USER'\] = '${DB_USER:-tendenci}'/" \
-        "$TENDENCI_PROJECT_ROOT/conf/settings.py"
-
-     sed -i "s/^#DATABASES\['default'\]\['PASSWORD'\].*/DATABASES\['default'\]\['PASSWORD'\] = '${DB_PASS:-password}'/" \
-        "$TENDENCI_PROJECT_ROOT/conf/settings.py"
-
-     sed -i "s/^#DATABASES\['default'\]\['PORT'\].*/DATABASES\['default'\]\['PORT'\] = '${DB_PORT:-5432}'/" \
-        "$TENDENCI_PROJECT_ROOT/conf/settings.py"
-
-     sed -i "s/TIME_ZONE.*/TIME_ZONE='${TIME_ZONE:-GMT+0}'/" \
-        "$TENDENCI_PROJECT_ROOT/conf/settings.py"
-
-     sed -i "s/ALLOWED_HOSTS =.*/ALLOWED_HOSTS = \[${ALLOWED_HOSTS:-'\*'}\]/" \
-        "$TENDENCI_PROJECT_ROOT/conf/settings.py"
-
-    echo "Finished creating settings" && echo ""
-}
-
 
 function create_superuser
 {
@@ -142,6 +100,13 @@ function initial_setup
 
 }
 
+function copy_config
+{
+  # Preparing directories
+  echo "Copying config from image to volume" && echo ""
+  cp -r "$APP_CONFIG_DIR"/* "$TENDENCI_PROJECT_ROOT"/conf
+}
+
 function run
 {
     cd "$TENDENCI_PROJECT_ROOT" \
@@ -153,9 +118,8 @@ if [ ! -f "$TENDENCI_PROJECT_ROOT/conf/first_run" ]; then
     check_user
     check_dirs
     create_tendenci_project
+    copy_config
     create_cronjobs
-    setup_keys
-    create_settings
     initial_setup
     run "$@"
 fi
